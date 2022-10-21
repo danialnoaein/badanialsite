36fdbdab5ddaea798bbd2f50e7ee8c3604c05a04
Danial <sdnoaein@gmail.com>
2022-10-21 21:42:48 +0330

    feat: :sparkles: add resume page

---
+++ pages/resume.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Resume = () => {
+  return (
+    <>
+      <Head>
+        <title>بادانیال - BaDanial : رزومه</title>
+      </Head>
+      <div className='container mt-8'>بزودی</div>
+    </>
+  );
+};
+
+export default Resume;
