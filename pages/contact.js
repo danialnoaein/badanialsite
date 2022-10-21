b7971875887cd18f81e3c6f21e935a10deaf4869
Danial <sdnoaein@gmail.com>
2022-10-21 21:42:31 +0330

    feat: :sparkles: add contact page

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Contact = () => {
+  return (
+    <>
+      <Head>
+        <title>بادانیال - BaDanial : تماس با من</title>
+      </Head>
+      <div className='container mt-8'>بزودی</div>
+    </>
+  );
+};
+
+export default Contact;
