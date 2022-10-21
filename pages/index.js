f47551e7ae6cb1ed420ff344f721d10bec7ca39e
Danial <sdnoaein@gmail.com>
2022-10-21 20:52:43 +0330

    feat: :sparkles: add article card in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
+import { useEffect } from "react";
+import ArticleCard from "../components/articles/ArticleCard";
+import { fetch as fetchPolyfill } from "whatwg-fetch";
+import { supabase } from "../data/supabaseClient";
+
 const Home = () => {
+  useEffect(() => {
+    getArticles();
+  });
+
   return (
     <>
-      <p>HOME</p>
+      <div className='container mt-8'>
+        <div>آخرین نوشته ها</div>
+
+        <div>
+          {}
+          <ArticleCard />
+        </div>
+      </div>
     </>
   );
 };
+
 export default Home;
+const getArticles = async () => {
+  try {
+    let { data: articles, error } = await supabase.from("Articles").select("*");
+  } catch (error) {
+    alert("Error loading user data!");
+    console.log(error);
+  }
+};
