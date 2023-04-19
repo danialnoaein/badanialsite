04d0fab86a38b5c7f846076bd6bbadda09d12e95
Danial Noaein <sdnoaein@gmail.com>
2023-04-19 11:03:34 +0330

    Delete next-sitemap.js

--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-// Default code you can customize according to your requirements.
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://badanial.site",
-  generateRobotsTxt: true, // (optional)
-};
